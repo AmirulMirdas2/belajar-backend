0aba992cd0a12d9b95352c0e9a40024014762d31
AmirulMirdas2 <amirul.mi@mhs.usk.ac.id>
2025-04-03 17:25:18 +0700

    feat: episode ke-6 = Setup Sequelize-CLI ExpressJS

--- belajar_di_youtube/belajar_express/app.js
+++ belajar_di_youtube/belajar_express/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv');
+const cors = require('cors');
 const categoriesRouter = require('./routes/categories')
+const morgan = require('morgan');
+
+dotenv.config();
 
 // middleware
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
 
 // routing
 app.use('/api/v1/categories',categoriesRouter);
 
 // listen web
+const port = process.env.PORT;
 app.listen(port, ()=> {
     console.log(`Server is running at http://localhost:${port}`);
-})
+})  
