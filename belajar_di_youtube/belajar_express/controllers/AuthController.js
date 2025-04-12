cca091715fccc44aaeb7d6d3f0fbbf450d44a174
AmirulMirdas2 <amirul.mi@mhs.usk.ac.id>
2025-04-12 10:47:13 +0700

    feat:EPS 25 = Refactory Auth (Send Response JWT Token Cookie)

--- belajar_di_youtube/belajar_express/controllers/AuthController.js
+++ belajar_di_youtube/belajar_express/controllers/AuthController.js
@@ -1,11 +1,39 @@
-const { User } = require("../models");
+const { User } = require("../models"); // import model User dari models bertujuan untuk mengakses database
 const jwt = require("jsonwebtoken");
 const signToken = (id) => {
+  // membuat token untuk user dimana id adalah id user yang baru saja dibuat
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user.id); // generate token dari id user yang baru saja dibuat
+  const cookieOptions = {
+    // membuat opsi cookie
+    expire: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000 // jam, menit, detik, milisecond
+    ),
+    httpOnly: true, // httpOnly adalah cookie yang hanya bisa diakses oleh server, bukan oleh client atau browser jadi hanya server yang bisa mengakses cookie ini
+  };
+
+  res.cookie("jwt", token, cookieOptions); // membuat coockie dengan nama jwt
+
+  user.password = undefined; // menghapus password dari user agar tidak ditampilkan di response
+
+  res.status(statusCode).json({
+    // jika berhasil maka akan menampilkan data berikut
+    status: "Succses",
+    token,
+    data: {
+      // data user
+      user,
+    },
+  });
+};
+
+// membuat fungsi registerUser yang digunakan untuk mendaftar user baru. fungsi ini akan menerima request dari client dan mengembalikan response ke client
+// dan akan membuat user baru di database
 exports.registerUser = async (req, res) => {
   try {
     if (req.body.password !== req.body.passwordConfirm) {
@@ -22,26 +50,18 @@ exports.registerUser = async (req, res) => {
       password: req.body.password, // mengambil data password dari body
     });
 
-    const token = signToken(newUser.id); // generate token dari id user yang baru saja dibuat
-
-    return res.status(201).json({
-      // mengembalikan response jika berhasil
-      status: "success",
-      token,
-      data: newUser,
-    });
+    createSendToken(newUser, 201, res); // menggunakan fungsi createSendToken untuk membuat token
   } catch (error) {
     console.log(error);
     return res.status(400).json({
       message: "Validasi Error",
-      error: error.errors.map((err) => err.message),
+      error: error.errors.map((err) => err.message), // error yang ditampilkan tidak panjang
     });
   }
 };
 
 exports.loginUser = async (req, res) => {
   // 1) fungsi validasi
-
   if (!req.body.email || !req.body.password) {
     return res.status(400).json({
       status: "fail",
@@ -63,11 +83,6 @@ exports.loginUser = async (req, res) => {
     });
   }
 
-  // 3) token di resopon pada saay login
-  const token = signToken(userData.id);
-  return res.status(200).json({
-    status: "success",
-    message: "Login Berhasil",
-    token,
-  });
+  // 3) token di resopon pada saat login
+  createSendToken(userData, 200, res); // menggunakan fungsi createSendToken untuk membuat token
 };
