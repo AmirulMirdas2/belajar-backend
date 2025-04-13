3837741c91c48e0b09d87387a4b0819c8f88fae9
AmirulMirdas2 <amirul.mi@mhs.usk.ac.id>
2025-04-14 06:22:00 +0700

    feat:EPS 29 = Get Current User

--- belajar_di_youtube/belajar_express/routes/AuthRouter.js
+++ belajar_di_youtube/belajar_express/routes/AuthRouter.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const {
-  registerUser,
-  loginUser,
-  logoutUser,
+  registerUser, // register user
+  loginUser, // login user
+  logoutUser, // logout user
+  getMyUser, // menampilkan data user
 } = require("../controllers/AuthController");
 const { authMiddleware } = require("../middleware/UserMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", authMiddleware, logoutUser);
+router.get("/me", authMiddleware, getMyUser);
 
 module.exports = router;
