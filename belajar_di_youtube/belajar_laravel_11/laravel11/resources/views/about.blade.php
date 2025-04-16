c2a64900fac995e860d741292b7709eed393d0cf
AmirulMirdas2 <amirul.mi@mhs.usk.ac.id>
2025-04-16 12:13:29 +0700

    feat:PES 6 = View Data

--- belajar_di_youtube/belajar_laravel_11/laravel11/resources/views/about.blade.php
+++ belajar_di_youtube/belajar_laravel_11/laravel11/resources/views/about.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Halaman About</title>
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
     <h1>Halaman About Mirdas</h1>
     <h3>Owner: {{ $nama }}</h3>
     <img src="image/foto.jpeg" alt="Mirdas Foto Ganteng" width="200px">
-</body>
-
-</html>
+</x-layout>
--- belajar_di_youtube/belajar_laravel_11/laravel11/routes/web.php
+++ belajar_di_youtube/belajar_laravel_11/laravel11/routes/web.php
@@ -1,19 +1,60 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view("home");
+    return view("home", ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Amirul Mirdas']);
+    return view('about', ['nama' => 'Amirul Mirdas'], ['title' => 'About Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog');
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id' => 1,
+            'sluq' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Amirul Mirdas',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, adipisci? Dolorum consequuntur saepe tenetur eaque sequi repellat voluptate hic, nisi amet est rem a facere tempora debitis repudiandae tempore voluptatem?'
+        ],
+        [
+            'id' => 2,
+            'sluq' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Amirul Mirdas',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, porro quam magnam consequatur exercitationem repellat nostrum! Aperiam inventore itaque distinctio iusto accusantium doloribus labore, dolorem, exercitationem quia minima, enim nihil.'
+        ],
+    ]]);
+});
+
+ROute::get('/posts/{sluq}', function ($sluq) {
+    $posts = [
+        [
+            'id' => 1,
+            'sluq' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Amirul Mirdas',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, adipisci? Dolorum consequuntur saepe tenetur eaque sequi repellat voluptate hic, nisi amet est rem a facere tempora debitis repudiandae tempore voluptatem?'
+        ],
+        [
+            'id' => 2,
+            'sluq' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Amirul Mirdas',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, porro quam magnam consequatur exercitationem repellat nostrum! Aperiam inventore itaque distinctio iusto accusantium doloribus labore, dolorem, exercitationem quia minima, enim nihil.'
+        ],
+
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($sluq) {
+        return $post['sluq'] == $sluq;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () { // buat di dalam folder views dengan nama file contact.blade.php 
-    return view('contact');
+    return view('contact', ['title' => 'Contact Page']);
 });
